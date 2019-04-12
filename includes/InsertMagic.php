5dc1fafbabcf4e8f7a7cd54a6f118c0ff31d3c0c
Umherirrender <umherirrender_de.wp@web.de>
2019-04-12 19:41:33 +0200

    Add mediawiki/mediawiki-codesniffer
    Using 25.0.0, because this extension required MediaWiki 1.31
    
    Change-Id: I0ac6ad67ab21fb3d7b6d6bceb313e3ce26872e7d

--- includes/InsertMagic.php
+++ includes/InsertMagic.php
@@ -4,4 +4,5 @@
  * @deprecated since version 3.1.0
  * Backwardscompatibility for static methods and variables
  */
-abstract class InsertMagic extends \BlueSpice\InsertMagic\Extension {}
+abstract class InsertMagic extends \BlueSpice\InsertMagic\Extension {
+}
